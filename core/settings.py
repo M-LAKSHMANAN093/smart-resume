929bd6d4e8c62661aac046b84feb4bc33711970e
Lakshmanan <lakshmananm093@gmail.com>
2025-11-13 23:04:36 +0530

    fix login redirect

--- core/settings.py
+++ core/settings.py
@@ -104,3 +104,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/resume/preview/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
